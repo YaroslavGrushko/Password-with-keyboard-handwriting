cb289fc7b58778878dac04063c87b0e08712a67c
YaroslavGrushko <Grushko.kpi@gmail.com>
2017-06-19 12:42:13 +0300

    Add files via upload

---
+++ Password/Password/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Password
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vector_of_durations = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LengthElapsed = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LengthElapsedSample = new System.Windows.Forms.Label();
+            this.ManhattanDistance = new System.Windows.Forms.Label();
+            this.Diff = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Vector_of_durations
+            // 
+            this.Vector_of_durations.AutoSize = true;
+            this.Vector_of_durations.Location = new System.Drawing.Point(206, 266);
+            this.Vector_of_durations.Name = "Vector_of_durations";
+            this.Vector_of_durations.Size = new System.Drawing.Size(0, 17);
+            this.Vector_of_durations.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(37, 38);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Enter User";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(37, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Check User";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Difference with the sample vector [%]:";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 18);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(73, 17);
+            this.PasswordLabel.TabIndex = 9;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(229, 18);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.Size = new System.Drawing.Size(117, 22);
+            this.password_textBox.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.password_textBox);
+            this.groupBox1.Controls.Add(this.PasswordLabel);
+            this.groupBox1.Location = new System.Drawing.Point(37, 179);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 53);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Please enter login and passward";
+            // 
+            // LengthElapsed
+            // 
+            this.LengthElapsed.AutoSize = true;
+            this.LengthElapsed.Location = new System.Drawing.Point(206, 384);
+            this.LengthElapsed.Name = "LengthElapsed";
+            this.LengthElapsed.Size = new System.Drawing.Size(0, 17);
+            this.LengthElapsed.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Vector of durations [ms]:";
+            // 
+            // LengthElapsedSample
+            // 
+            this.LengthElapsedSample.AutoSize = true;
+            this.LengthElapsedSample.Location = new System.Drawing.Point(206, 401);
+            this.LengthElapsedSample.Name = "LengthElapsedSample";
+            this.LengthElapsedSample.Size = new System.Drawing.Size(0, 17);
+            this.LengthElapsedSample.TabIndex = 15;
+            // 
+            // ManhattanDistance
+            // 
+            this.ManhattanDistance.AutoSize = true;
+            this.ManhattanDistance.Location = new System.Drawing.Point(206, 418);
+            this.ManhattanDistance.Name = "ManhattanDistance";
+            this.ManhattanDistance.Size = new System.Drawing.Size(0, 17);
+            this.ManhattanDistance.TabIndex = 16;
+            // 
+            // Diff
+            // 
+            this.Diff.AutoSize = true;
+            this.Diff.Location = new System.Drawing.Point(285, 283);
+            this.Diff.Name = "Diff";
+            this.Diff.Size = new System.Drawing.Size(0, 17);
+            this.Diff.TabIndex = 17;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(37, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(364, 53);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Login:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 37);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Clear Users";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Identification result:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 17);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Duration of Sample [ms]:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 384);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(175, 17);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Duration of Verifiable [ms]:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(34, 420);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(168, 17);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Manhattan Distance [ms]:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 485);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Diff);
+            this.Controls.Add(this.ManhattanDistance);
+            this.Controls.Add(this.LengthElapsedSample);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LengthElapsed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Vector_of_durations);
+            this.Name = "Form1";
+            this.Text = "Biometric Password";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Vector_of_durations;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LengthElapsed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LengthElapsedSample;
+        private System.Windows.Forms.Label ManhattanDistance;
+        private System.Windows.Forms.Label Diff;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Password/Password/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Password
+{
+    public partial class Form1 : Form
+    {
+        public static List<long> elapsedList = new List<long>();
+        public static List<long> elapsedListSample = new List<long>();
+        long Length_elapsed = 0;              //Length of user's vector
+        long Length_elapsed_Sample = 0;       //Length of sample's vector:
+        long Manhattan_distance = 0;
+        double Difference_with_the_sample = 0;
+               
+        public Form1()
+        {           
+            InitializeComponent();
+            KeyPreview = true;
+            
+            var watch = new Stopwatch();
+            KeyDown += (s, e) => watch.Start();
+            KeyUp += (s, e) =>
+            {                
+                if (radioButton1.Checked)   //enter of user is selected
+                {
+                    watch.Stop();
+                    if (password_textBox.Focused)
+                    {
+                        var elapsed = watch.ElapsedMilliseconds;    // Duration
+                        elapsedListSample.Add(elapsed);
+                        Vector_of_durations.Text += " " + elapsed;
+                    }
+                    watch.Reset();                  
+                }
+                else   //check of user is selected
+                {
+                    watch.Stop();
+                    if (password_textBox.Focused)
+                    {
+                        var elapsed = watch.ElapsedMilliseconds;    // Duration
+                        elapsedList.Add(elapsed);
+                        Vector_of_durations.Text += " " + elapsed;
+                    }
+                    watch.Reset();
+                }           
+            };                                            
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //  Apply button
+        {
+            Vector_of_durations.Text = "";
+            Length_elapsed_Sample = 0;
+            
+            if (radioButton1.Checked)   //enter of user is selected
+            {
+                var vect = textBox1.Text; //Login
+                var pass = password_textBox.Text;
+
+                password_textBox.Text = "";
+
+                    for (int i = 0; i < elapsedListSample.Count; i++)
+                    {
+                        Length_elapsed_Sample += elapsedListSample[i];    //Length of user's vector
+                        vect =vect+ '|' + elapsedListSample[i].ToString();
+                    }
+                        LengthElapsedSample.Text = Length_elapsed_Sample.ToString();
+                        vect += "|password=" + pass;
+                //"UsersHandwriting" variable in config file for saving data:
+                ((System.Collections.Specialized.StringCollection)Password.Properties.Settings.Default["UsersHandwriting"]).Add(vect);
+                Password.Properties.Settings.Default.Save();
+
+                textBox1.Text = ""; //Clear Login
+                elapsedListSample.Clear();
+            }
+            else {
+                Length_elapsed = 0;
+               for (int i = 0; i < elapsedList.Count; i++)
+               {
+                    Length_elapsed += elapsedList[i];   //Length of sample's vector
+               }
+                    LengthElapsed.Text = Length_elapsed.ToString();
+        
+                elapsedListSample.Clear();
+
+                int c = ((System.Collections.Specialized.StringCollection)Password.Properties.Settings.Default["UsersHandwriting"]).Count;
+                String[,] myUsersDiff = new String[c,3];
+                
+                int index = 0;   //myUsersDiff index
+
+            foreach (var user in ((System.Collections.Specialized.StringCollection)Password.Properties.Settings.Default["UsersHandwriting"]))
+            {
+                string[] items = user.Split('|');
+                myUsersDiff[index,0] = items[0];  //user's Name
+                int itemsIndex = 0;               //items index
+
+                elapsedListSample.Clear();
+
+                foreach (string item in items)
+                {
+                    if (itemsIndex != 0) {
+                            if (!item.Contains("password=")) { 
+                                elapsedListSample.Add(Convert.ToInt64(item));
+                            }else {
+
+                                var pass = item.Replace("password=", "");
+                                if (pass == password_textBox.Text) { myUsersDiff[index, 2] = "1"; }  //verify password
+                                else
+                                {
+                                    myUsersDiff[index, 2] = "0";
+                                };
+                                  }
+                            }
+                   itemsIndex++;
+                }
+                    Length_elapsed_Sample = 0;
+                    for (int i = 0; i < elapsedListSample.Count; i++)
+                    {
+                        Length_elapsed_Sample += elapsedListSample[i];  //Length of user's vector
+                    }
+
+                      if ((Length_elapsed > 0) && (Length_elapsed_Sample > 0))  //check of user and Sample are exist
+                        {
+                      if(  elapsedListSample.Count== elapsedList.Count)  //verify vectors length
+                        { 
+
+                    for (int i = 0; i < elapsedListSample.Count; i++)
+                    {
+                        Manhattan_distance += Math.Abs(elapsedListSample[i] - elapsedList[i]);  //Manhattan distance                                
+                    }
+                        }else { myUsersDiff[index, 2] = "0"; }
+
+                        ManhattanDistance.Text = Manhattan_distance.ToString();
+                        Difference_with_the_sample = ((float)Manhattan_distance / (float)(Length_elapsed + Length_elapsed_Sample)) * 100;
+                        myUsersDiff[index,1] = Difference_with_the_sample.ToString();
+                       
+                        }
+                index++;
+            }
+
+            double minDiff = 1000;
+            String ResultUser = "";
+            for(int ind=0;ind< myUsersDiff.GetLength(0); ind++)
+            {
+                if ((minDiff > Convert.ToDouble(myUsersDiff[ind,1]))&&(textBox1.Text==myUsersDiff[ind, 0])&& myUsersDiff[ind,2]=="1")
+                    { minDiff = Convert.ToDouble(myUsersDiff[ind, 1]);
+                      ResultUser = myUsersDiff[ind, 0];
+                    }
+            }
+            Diff.Text = minDiff.ToString("F2");
+
+                if (ResultUser != "") { label6.Text = ResultUser; }
+                else { label6.Text = "UserName or password is incorrect"; }
+                
+            LengthElapsedSample.Text = Length_elapsed_Sample.ToString();
+
+            password_textBox.Clear();
+            elapsedListSample.Clear();
+            elapsedList.Clear();
+            Length_elapsed = 0;
+            Length_elapsed_Sample = 0;
+            Manhattan_distance = 0;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Vector_of_durations.Text = "";
+            if (radioButton1.Checked)
+            {
+                elapsedList.Clear();
+                elapsedListSample.Clear();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+     
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)  //move to check user
+        {
+            if (password_textBox.Text != "" && textBox1.Text != "")
+            { 
+            var vect = textBox1.Text; //Login
+            if (radioButton2.Checked)
+                {
+                 var pass = password_textBox.Text;
+                 password_textBox.Text = "";
+                
+                for (int i = 0; i < elapsedListSample.Count; i++)
+                {
+                Length_elapsed_Sample += elapsedListSample[i];    //Length of user's vector
+                    vect+= '|' + elapsedListSample[i];
+                }
+                vect += "password=" + pass;
+                LengthElapsedSample.Text =  Length_elapsed_Sample.ToString();
+                }
+
+            ((System.Collections.Specialized.StringCollection)Password.Properties.Settings.Default["UsersHandwriting"]).Add(vect);
+            Password.Properties.Settings.Default.Save();
+            textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ((System.Collections.Specialized.StringCollection)Password.Properties.Settings.Default["UsersHandwriting"]).Clear();
+        }
+    }
+}
